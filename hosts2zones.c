051d788264a5aabe42db1448b7c6c7fceeac14eb
Rolf Jansen <github@cyclaero.com>
2016-11-14 16:36:42 -0200

    formatting

--- hosts2zones.c
+++ hosts2zones.c
@@ -34,12 +34,14 @@
 #include "binutils.h"
 #include "store.h"
 
+
 #define hcnt 65535
 
 int     hosts_count = 0;
 int     zones_count = 0;
 FILE   *in, *out;
 Node  **domainStore;
+
 
 void zoneOutput(Node *node)
 {
@@ -76,47 +78,53 @@ int main(int argc, const char *argv[])
       for (inc = 2; inc < argc; inc++)
          if (stat(argv[inc], &st) == noerr && st.st_size > 3 && (in = fopen(argv[inc], "r")))
          {
-            char *hosts = allocate(st.st_size+2, false);
+            char *hosts = allocate(st.st_size + 2, false);
             if (fread(hosts, st.st_size, 1, in) == 1)
             {
-               *(uint16_t *)&hosts[st.st_size] = *(uint16_t *)"\n";
+               *(uint16_t *)&hosts[st.st_size] = *(uint16_t *)"\n";                    // guaranteed end of line + end of string at the end of the read-in data
 
                bool  iswhite;
-               int   ll, wl;
-               char *word, *lineend, *line = hosts + blanklen(hosts);
-               char *nextword, *nextline;
+               int   dl, ll, wl;
+               char *word, *lineend, *nextword, *nextline;
+               char *line = hosts + blanklen(hosts);                                   // get the start of the first line by skipping leading blanks + blank lines
 
                while (line < hosts + st.st_size)
                {
-                  ll = linelen(line); *(lineend = bskip(line+ll)) = '\0';              // skip trailing blanks
+                  ll = linelen(line);
+                  lineend   = bskip(line+ll);                                          // skip trailing blanks
+                  *lineend  = '\0';                                                    // separate the entry as string
+
                   nextline  = line + ll+1;                                             // get the start of the next line
-                  nextline += blanklen(nextline);                                      // by skipping leading blanks and blank lines
+                  nextline += blanklen(nextline);                                      // by skipping leading blanks + blank lines
 
                   if (*line != '#')                                                    // skip comment lines
                   {
-                     word = NULL;                                                      // assume inacceptable entry
+                     word = NULL;                                                      // assume an inacceptable entry
 
-                     wl = wordlen(line); line[wl] = '\0';                              // pickup the first entry on the line
-                     if (line+wl == lineend && *line != '.' && domainlen(line) < wl)   // accept a single domain name per line that must contain at least 1 non-leading/non-traling dot
-                        iswhite = false, word = line;                                  // simple domain lists are always black lists
-
-                     else if ((iswhite = *(int64_t *)line == *(int64_t *)"1.1.1.1") || // otherwise assume the Hosts file format
-                             /*isblack*/ *(int64_t *)line == *(int64_t *)"0.0.0.0"  || // entrie starting with 1.1.1.1 shall be white listed
-                             /*isblack*/ *(int16_t *)line == *(int16_t *)"12" &&       // 0.0.0.0 or 127.0.0.1 are black list entries
+                     line[wl = wordlen(line)] = '\0';                                  // pickup the first entry on the line
+                     if (line+wl == lineend &&                                         // accept a single domain name per line which
+                         0 < (dl = domainlen(line)) && dl < wl)                        // must contain at least 1 non-leading & non-traling dot
+                        word = line, iswhite = false;                                  // simple domain lists are always black lists
+                                                                                       // otherwise assume the Hosts file format
+                     else if ((iswhite = *(int64_t *)line == *(int64_t *)"1.1.1.1") || // entries starting with 1.1.1.1 shall be white listed
+                             /*isblack*/ *(int64_t *)line == *(int64_t *)"0.0.0.0"  || // 0.0.0.0 or 127.0.0.1 are black list entries
+                             /*isblack*/ *(int16_t *)line == *(int16_t *)"12" &&
                                     *(int64_t *)(line+=2) == *(int64_t *)"7.0.0.1")
-                        word = line + 8, word += blanklen(word);                       // skip address and leading blanks
+                        word = line + 8, word += blanklen(word);                       // skip IP address and leading blanks
 
-                     if (word)                                                         // process only simple domain entries or entries in Hosts file format
-                        while (word && *word && *word != '#' && word < nextline)       // Hosts files may contain more than one domain per line
+                     if (word)                                                         // only process simple domain entries or entries in Hosts file format
+                        while (*word && *word != '#' && word < nextline)               // Hosts files may contain more than one domain per line
                         {
                            wl = wordlen(word);
                            nextword = word + wl;
                            *nextword++ = '\0';
                            nextword += blanklen(nextword);
 
-                           if (*lowercase(word, wl) &&                                 // convert to lowercase, end check if it is a non empty
-                               (wl != 9 || *word != 'l' || *(int64_t *)(word+1) != *(int64_t *)"ocalhost") && !findName(domainStore, word, wl))
-                           {
+                           if (*lowercase(word, wl) &&                                 // convert to lowercase, end check if it is a non empty string
+                               (*(int64_t *)word != *(int64_t *)"localhos"             // don't process 'localhost' entries
+                                || *(int16_t *)(word+8) != *(int16_t *)"t") &&
+                               !findName(domainStore, word, wl))                       // if the entry does not exit in the domain store
+                           {                                                           // then create a new one for the given domain name
                               Value value = {Simple, .b = iswhite};
                               storeName(domainStore, word, wl, &value);
                               hosts_count++;
@@ -144,6 +152,6 @@ int main(int argc, const char *argv[])
 
       return (zones_count > 0) ? 0 : 1;
    }
-
-   return 1;
+   else
+      return 1;
 }
